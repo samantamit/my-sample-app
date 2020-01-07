a169c8de4fc53d80749e43547ad578729a71ce5f
Amit Samant <amsamant@paypal.com>
2020-01-07 14:17:39 -0600

    Updates

---
+++ precache-manifest.dc60625e5b1871b4dd03bccd737d23b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6309b47b7ec11c83bcdd4b2a07996f",
+    "url": "/my-sample-app/index.html"
+  },
+  {
+    "revision": "21f421dc9f71480061af",
+    "url": "/my-sample-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9f6d53959311150317d",
+    "url": "/my-sample-app/static/js/2.88600d35.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-sample-app/static/js/2.88600d35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21f421dc9f71480061af",
+    "url": "/my-sample-app/static/js/main.a0c0dee1.chunk.js"
+  },
+  {
+    "revision": "b86832a192e00ee7d753",
+    "url": "/my-sample-app/static/js/runtime-main.0c1d56c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-sample-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
